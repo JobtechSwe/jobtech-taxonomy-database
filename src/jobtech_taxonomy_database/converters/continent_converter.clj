bb977b4b3eeeb398334f29fb85b33499808de84b
SickanEkman <sara.d.ekman@gmail.com>
2019-01-11 16:52:50 +0100

    Add continent converter

---
+++ src/jobtech_taxonomy_database/converters/continent_converter.clj
@@ -0,0 +1,33 @@
+(ns jobtech-taxonomy-database.converters.continent-converter
+  (:gen-class)
+  (:require [datomic.client.api :as d]
+            [jobtech-taxonomy-database.schema :refer :all :as schema]
+            [jobtech-taxonomy-database.legacy-migration :refer :all]
+            [jobtech-taxonomy-database.config :refer :all]
+            [jobtech-taxonomy-database.datomic-connection :refer :all :as conn]))
+
+
+(defn ^:private fake-id
+  "Temporary function until new id function with nano-ids is imported into this project."
+  [id]
+  (format "%010d" id))
+
+
+(defn converter
+  "Immutable language converter."
+  [data]
+  [{:concept/id (str  (:continentid  data))
+    :concept/description       (:term data)
+    :concept/preferred-term     (str  (:continentid  data))
+    :concept/alternative-terms #{(str  (:continentid  data))}}
+   {:db/id  (str  (:continentid  data))
+    :term/base-form (:term data)}])
+
+
+(defn convert [] (mapcat converter  (fetch-data get-continents)))
+
+
+(defn writer
+  [data]
+
+  (d/transact (get-conn) {:tx-data data}))
