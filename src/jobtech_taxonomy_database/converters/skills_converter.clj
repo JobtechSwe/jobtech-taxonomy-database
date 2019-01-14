fd46bdc027689cf655b42877649c2dd0478e7e08
Per Weijnitz <per.weijnitz@gmail.com>
2019-01-14 10:55:21 +0100

    now we use a kind of interface for converters: convert writer

--- src/jobtech_taxonomy_database/converters/skills_converter.clj
+++ src/jobtech_taxonomy_database/converters/skills_converter.clj
@@ -12,62 +12,53 @@
   (format "%010d" id))
 
 
-;;(defn keep-unique-db-id
-;;  "Remove duplicate concepts"
-;;  [[head & tail] list
-;;   mem]
-;;  (let 
-;;  (if head
-;;    
 
 
-(defn skill-converter
+(defn converter
   "Immutable skill converter."
-  [skills]
+  [skill]
 
-  (vec
-   (apply concat
-          (apply concat
-                 (map (fn [skill]
-                        (let* [skill-main-headline (get skill :main-headline)
-                               headlines (get skill :headlines)
-                               term-mainhl (get skill-main-headline :term)
-                               skill-main-headline-id (get skill-main-headline :skillmainheadlineid)
-                               headline-output (map (fn [skill-headline]
-                                                      (let* [term-hl (get skill-headline :term)
-                                                             skill-headline-id (get skill-headline :skillheadlineid)
-                                                             concept-hl {:db/id                     (fake-id skill-headline-id)
-                                                                         :concept/id                (fake-id skill-headline-id)
-                                                                         :concept/description       "zkrpkt"
-                                                                         :concept/preferred-term    term-hl
-                                                                         :concept/alternative-terms term-hl}
-                                                             relation {:relation/concept-1 (fake-id skill-main-headline-id)
-                                                                       :relation/concept-2 (fake-id skill-headline-id)
-                                                                       :relation/type      :hyponym}]
-                                                            (list {:db/id  term-mainhl
-                                                                   :term/base-form term-mainhl}
-                                                                  {:db/id  term-hl
-                                                                   :term/base-form term-hl}
-                                                                  concept-hl
-                                                                  relation)))
-                                                    headlines)
-                               output (list (list {:db/id                     (fake-id skill-main-headline-id)
-                                                   :concept/id                (fake-id skill-main-headline-id)
-                                                   :concept/description       "hrpf"
-                                                   :concept/preferred-term    term-mainhl
-                                                   :concept/alternative-terms term-mainhl}))]
-                              (concat output headline-output)))
-                      skills)))))
+  (let* [skill-main-headline (get skill :main-headline)
+         headlines (get skill :headlines)
+         term-mainhl (get skill-main-headline :term)
+         skill-main-headline-id (get skill-main-headline :skillmainheadlineid)
+         headline-output (map (fn [skill-headline]
+                                (let* [term-hl (get skill-headline :term)
+                                       skill-headline-id (get skill-headline :skillheadlineid)
+                                       concept-hl {:db/id                     (fake-id skill-headline-id)
+                                                   :concept/id                (fake-id skill-headline-id)
+                                                   :concept/description       "zkrpkt"
+                                                   :concept/preferred-term    term-hl
+                                                   :concept/alternative-terms term-hl}
+                                       relation {:relation/concept-1 (fake-id skill-main-headline-id)
+                                                 :relation/concept-2 (fake-id skill-headline-id)
+                                                 :relation/type      :hyponym}]
+                                  (list {:db/id  term-mainhl
+                                         :term/base-form term-mainhl}
+                                        {:db/id  term-hl
+                                         :term/base-form term-hl}
+                                        concept-hl
+                                        relation)))
+                              headlines)
+         output (list (list {:db/id                     (fake-id skill-main-headline-id)
+                             :concept/id                (fake-id skill-main-headline-id)
+                             :concept/description       "hrpf"
+                             :concept/preferred-term    term-mainhl
+                             :concept/alternative-terms term-mainhl}))]
+    (concat output headline-output)))
 
-(defn skill-retriever
-  "Get all skills from the old database."
+
+(defn convert
+  ""
   []
-  (map (fn [skill-main-headline]
-         (let [skill-main-headline-id (get skill-main-headline :skillmainheadlineid)]
-           (hash-map :main-headline skill-main-headline
-                     :headlines (list (get-skillheadlines {:id skill-main-headline-id})))))
-       (get-skillmainheadlines)))
+  (apply concat
+         (mapcat converter
+                 (map (fn [skill-main-headline]
+                        (let [skill-main-headline-id (get skill-main-headline :skillmainheadlineid)]
+                          (hash-map :main-headline skill-main-headline
+                                    :headlines (list (get-skillheadlines {:id skill-main-headline-id})))))
+                      (get-skillmainheadlines)))))
 
-(defn skill-writer
+(defn writer
   [data]
   (d/transact (get-conn) {:tx-data data}))
--- src/jobtech_taxonomy_database/core.clj
+++ src/jobtech_taxonomy_database/core.clj
@@ -4,15 +4,33 @@
             [clojure.pprint :as pp]
             [jobtech-taxonomy-database.schema :refer :all :as schema]
             [jobtech-taxonomy-database.legacy-migration :refer :all]
-            [jobtech-taxonomy-database.datomic-connection :refer :all :as conn]
-            [jobtech-taxonomy-database.converters.skills-converter :refer :all]))
+            [jobtech-taxonomy-database.datomic-connection :refer :all]
+            [jobtech-taxonomy-database.converters.skills-converter]
+            [jobtech-taxonomy-database.converters.continent-converter]
+            ;;[jobtech-taxonomy-database.converters.country-converter]
+            ;;[jobtech-taxonomy-database.converters.drivers-license-converter]
+            ;;[jobtech-taxonomy-database.converters.employment-duration-converter]
+            ;;[jobtech-taxonomy-database.converters.language-converter]
+            ;;[jobtech-taxonomy-database.converters.language-level-converter]
+            ;;[jobtech-taxonomy-database.converters.skills-converter]
+            ;;[jobtech-taxonomy-database.converters.worktime-extent-converter]
+            ))
 
 (def converters
   "Each logic section of the old taxonomy can be handled by a converter
   set consisting of a reader, converter, writer and their
   namespace. By making the converter immutable it becomes easier to
   test. Add new converter sets here."
-  '({:reader skill-retriever :converter skill-converter :writer skill-writer :namespace jobtech-taxonomy-database.converters.skills-converter}))
+  '({:namespace jobtech-taxonomy-database.converters.skills-converter}
+    {:namespace jobtech-taxonomy-database.converters.continent-converter}
+    ;;{:namespace jobtech-taxonomy-database.converters.country-converter}
+    ;;{:namespace jobtech-taxonomy-database.converters.drivers-license-converter}
+    ;;{:namespace jobtech-taxonomy-database.converters.employment-duration-converter}
+    ;;{:namespace jobtech-taxonomy-database.converters.language-converter}
+    ;;{:namespace jobtech-taxonomy-database.converters.language-level-converter}
+    ;;{:namespace jobtech-taxonomy-database.converters.skills-converter}
+    ;;{:namespace jobtech-taxonomy-database.converters.worktime-extent-converter}
+    ))
 
 (defn -main
   []
@@ -21,9 +39,8 @@
 
   (println "**** read from old taxonomy db, convert, and write to datomic...")
   (run! (fn [x]
-          (let [input-data ((ns-resolve (get x :namespace) (get x :reader)))
-                converted-data ((ns-resolve (get x :namespace) (get x :converter)) input-data)]
-            ((ns-resolve (get x :namespace) (get x :writer)) converted-data)))
+          (let [converted-data ((ns-resolve (get x :namespace) 'convert))]
+            ((ns-resolve (get x :namespace) 'writer) converted-data)))
         converters)
 
   (println "**** basic queries to datomic to see some results...")
