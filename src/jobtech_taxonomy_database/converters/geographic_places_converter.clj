6851bc4f937de47f1945ee2c13c5e1dcbd5298e1
SickanEkman <sara.d.ekman@gmail.com>
2019-02-13 16:31:32 +0100

    Geographic converter working in general. Can't handle inconsistencies on region level still.

--- src/jobtech_taxonomy_database/converters/geographic_places_converter.clj
+++ src/jobtech_taxonomy_database/converters/geographic_places_converter.clj
@@ -21,18 +21,22 @@
         id-67-region (keyword (str (:region-eu-id data)))
         term-67-continent (:continent-term data)
         term-67-country (:country-term data)
-        term-67-region (:region-eu-term data)]                        ;ska matcha preferredTerm i json
-    (let [nano-id-continent (get-nano category-67-continent id-67-continent term-67-continent)
-          nano-id-country (get-nano category-67-country id-67-country term-67-country)
-          nano-id-region (get-nano category-67-region id-67-region term-67-region)]
-      (list
+        term-67-region (:region-eu-term data)                        ;ska matcha preferredTerm i json
+        nano-id-continent (get-nano category-67-continent id-67-continent term-67-continent)
+        nano-id-country (get-nano category-67-country id-67-country term-67-country)
+        nano-id-region (get-nano category-67-region id-67-region term-67-region)]
+    (conj
+      (when (not= (:continent-id data) nil)
+        (list
         {:concept/id                nano-id-continent
         :concept/description       term-67-continent
         :concept/preferred-term    nano-id-continent
          :concept/category         category-67-continent}
        {:db/id          nano-id-continent
-        :term/base-form term-67-continent}
-       {:relation/concept-1     nano-id-continent
+        :term/base-form term-67-continent}))
+        (when (not= (:country-id data) nil)
+          (list
+        {:relation/concept-1     nano-id-continent
         :relation/concept-2     nano-id-country
         :relation/type          :continent-to-country}
        {:concept/id                nano-id-country
@@ -40,7 +44,9 @@
         :concept/preferred-term    nano-id-country
         :concept/category           category-67-country}
        {:db/id          nano-id-country
-        :term/base-form term-67-country}
+        :term/base-form term-67-country}))
+       (when (not= (:region-eu-id data) nil)
+         (list
        {:relation/concept-1     nano-id-country
         :relation/concept-2     nano-id-region
         :relation/type          :country-to-region-eu}
@@ -51,7 +57,7 @@
         :concept.external-standard/nuts-code (:region-nuts-code-level-3 data)}
        {:db/id          nano-id-region
         :term/base-form term-67-region}
-        ))))
+          )))))
 
 (def sql-answer-with-nuts
   {:continent-id 2,
@@ -93,3 +99,5 @@
   ""
   []
   (converter sql-answer-no-nuts))
+
+;TODO Continue with logic for eu-regions with only code or only name or only NUTS or something
