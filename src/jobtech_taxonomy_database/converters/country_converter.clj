f25a9ea8fd2a33522e79346454a2847f58ca787f
Per Weijnitz <per.weijnitz@gmail.com>
2019-03-06 13:12:36 +0100

    Add legacy ID.

--- src/jobtech_taxonomy_database/converters/country_converter.clj
+++ src/jobtech_taxonomy_database/converters/country_converter.clj
@@ -13,12 +13,13 @@
   "Immutable language converter."
   [data]
   (let [category-67 :country                                ;json-nyckeln
-        id-67 (keyword (str (:countryid data)))             ;ska matcha legacyAmsTaxonomyId i json
+        id-67 (str (:countryid data))             ;ska matcha legacyAmsTaxonomyId i json
         description-67 (:term data)]                        ;ska matcha preferredTerm i json
-    (let [nano-id (get-nano category-67 id-67)]
+    (let [nano-id (get-nano category-67 (keyword id-67))]
       [{:concept/id                nano-id
         :concept/description       description-67
         :concept/preferred-term    nano-id
+        :concept.taxonomy-67-id    id-67
         :concept/category          category-67}
        {:db/id          nano-id
         :term/base-form description-67}])))
