a164e722ea2d8514a9b3c9c713bc2c338e3e39ff
Per Weijnitz <per.weijnitz@gmail.com>
2019-03-07 07:27:22 +0100

    Tidy up.

--- src/jobtech_taxonomy_database/core.clj
+++ src/jobtech_taxonomy_database/core.clj
@@ -5,15 +5,17 @@
             [jobtech-taxonomy-database.schema :refer :all :as schema]
             [jobtech-taxonomy-database.legacy-migration :refer :all]
             [jobtech-taxonomy-database.datomic-connection :refer :all]
-            [jobtech-taxonomy-database.converters.skills-converter]
             [jobtech-taxonomy-database.converters.continent-converter]
-            ;;[jobtech-taxonomy-database.converters.country-converter]
-            ;;[jobtech-taxonomy-database.converters.drivers-license-converter]
-            ;;[jobtech-taxonomy-database.converters.employment-duration-converter]
-            ;;[jobtech-taxonomy-database.converters.language-converter]
-            ;;[jobtech-taxonomy-database.converters.language-level-converter]
-            ;;[jobtech-taxonomy-database.converters.skills-converter]
-            ;;[jobtech-taxonomy-database.converters.worktime-extent-converter]
+            [jobtech-taxonomy-database.converters.country-converter]
+            [jobtech-taxonomy-database.converters.drivers-license-converter]
+            [jobtech-taxonomy-database.converters.employment-duration-converter]
+            [jobtech-taxonomy-database.converters.employment-type-converter]
+            [jobtech-taxonomy-database.converters.geographic-places-converter]
+            [jobtech-taxonomy-database.converters.language-converter]
+            [jobtech-taxonomy-database.converters.language-level-converter]
+            [jobtech-taxonomy-database.converters.skills-converter]
+            [jobtech-taxonomy-database.converters.wage-type-converter]
+            [jobtech-taxonomy-database.converters.worktime-extent-converter]
             ))
 
 (def converters
@@ -21,16 +23,20 @@
   set consisting of a reader, converter, writer and their
   namespace. By making the converter immutable it becomes easier to
   test. Add new converter sets here."
-  '({:namespace jobtech-taxonomy-database.converters.skills-converter}
-    {:namespace jobtech-taxonomy-database.converters.continent-converter}
-    ;;{:namespace jobtech-taxonomy-database.converters.country-converter}
-    ;;{:namespace jobtech-taxonomy-database.converters.drivers-license-converter}
-    ;;{:namespace jobtech-taxonomy-database.converters.employment-duration-converter}
-    ;;{:namespace jobtech-taxonomy-database.converters.language-converter}
-    ;;{:namespace jobtech-taxonomy-database.converters.language-level-converter}
-    ;;{:namespace jobtech-taxonomy-database.converters.skills-converter}
-    ;;{:namespace jobtech-taxonomy-database.converters.worktime-extent-converter}
+  '(
+     {:namespace jobtech-taxonomy-database.converters.continent-converter}
+     {:namespace jobtech-taxonomy-database.converters.country-converter}
+     {:namespace jobtech-taxonomy-database.converters.drivers-license-converter}
+     {:namespace jobtech-taxonomy-database.converters.employment-duration-converter}
+     {:namespace jobtech-taxonomy-database.converters.employment-type-converter}
+     ;;{:namespace jobtech-taxonomy-database.converters.geographic-places-converter}
+     {:namespace jobtech-taxonomy-database.converters.language-converter}
+     {:namespace jobtech-taxonomy-database.converters.language-level-converter}
+     {:namespace jobtech-taxonomy-database.converters.skills-converter}
+     {:namespace jobtech-taxonomy-database.converters.wage-type-converter}
+     {:namespace jobtech-taxonomy-database.converters.worktime-extent-converter}
     ))
+
 
 (defn -main
   []
@@ -38,14 +44,14 @@
   (init-new-db)
 
   (println "**** read from old taxonomy db, convert, and write to datomic...")
-  (run! (fn [x]
-          (println "**** Calling " (get x :namespace))
-          (let [converted-data ((ns-resolve (get x :namespace) 'convert))]
-            (d/transact (get-conn) {:tx-data converted-data})))
-        converters)
-
-  (println "**** basic queries to datomic to see some results...")
-  (clojure.pprint/pprint (d/q '[:find ?x :where [_ :term/base-form ?x]] (get-db)))
-  (clojure.pprint/pprint (d/q '[:find ?x :where [_ :concept/id ?x]] (get-db)))
-  (clojure.pprint/pprint (d/q '[:find ?x :where [_ :relation/concept-1 ?x]] (get-db))))
+  (set! *print-length* 100000)
+  (with-open [w (clojure.java.io/writer "/tmp/latest-convert-for-debug.edn")]
+    (binding [*out* w]
+      (run! (fn [x]
+              ;;(println "**** Calling " (get x :namespace))
+              (let [converted-data ((ns-resolve (get x :namespace) 'convert))]
+                (pp/write converted-data)
+                (d/transact (get-conn) {:tx-data converted-data})))
+            converters))))
+;; (-main)
 ;Should we add a source attribute to the transaction? See https://docs.datomic.com/on-prem/best-practices.html#add-facts-about-transaction-entity
