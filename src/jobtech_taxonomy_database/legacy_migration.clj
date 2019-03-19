fc056ae1e5d847c6d72ffdb9f687dfac4af4770a
SickanEkman <sara.d.ekman@gmail.com>
2019-03-19 11:26:37 +0100

    Remove duplicate code (looked like error, hope it wasn't intentional)

--- src/jobtech_taxonomy_database/legacy_migration.clj
+++ src/jobtech_taxonomy_database/legacy_migration.clj
@@ -5,25 +5,6 @@
 
 (hugsql/def-db-fns "jobtech_taxonomy_database/sql/legacy-taxonomy.sql")
 
-(hugsql/def-sqlvec-fns "jobtech_taxonomy_database/sql/legacy-taxonomy.sql")(ns jobtech-taxonomy-database.legacy-migration
-  (:gen-class)
-  (:require [hugsql.core :as hugsql]
-            [jobtech-taxonomy-database.config :refer :all]))
-
-(hugsql/def-db-fns "jobtech_taxonomy_database/sql/legacy-taxonomy.sql")
-
-(hugsql/def-sqlvec-fns "jobtech_taxonomy_database/sql/legacy-taxonomy.sql")
-
-(defn fetch-data [hugsql-function & args]
-  "Call database with a hugsql function"
-  (hugsql-function (get-legacydb-config) (if args (first args) nil)))
-(ns jobtech-taxonomy-database.legacy-migration
-  (:gen-class)
-  (:require [hugsql.core :as hugsql]
-            [jobtech-taxonomy-database.config :refer :all]))
-
-(hugsql/def-db-fns "jobtech_taxonomy_database/sql/legacy-taxonomy.sql")
-
 (hugsql/def-sqlvec-fns "jobtech_taxonomy_database/sql/legacy-taxonomy.sql")
 
 (defn fetch-data [hugsql-function & args]
