1844a563f31ce3acda2397c5e0bd1540ce2e98e2
Henrik Suzuki <6067421+qeshi@users.noreply.github.com>
2019-03-20 15:42:50 +0100

    Merge branch 'master' of https://github.com/JobtechSwe/jobtech-taxonomy-database

--- src/jobtech_taxonomy_database/config.clj
+++ src/jobtech_taxonomy_database/config.clj
@@ -4,12 +4,27 @@
 ;; TODO: load from separate, profile specific, properties files (dev/prod)
 ;;  ... (delay (load-file (.getFile (resource "config.clj"))))
 
-(def ^:private datomic-config
+#_(def ^:private datomic-config
   {:datomic-name "taxonomy_v13"
    :datomic-cfg {:server-type :peer-server
                  :access-key  "myaccesskey"
                  :secret      "mysecret"
                  :endpoint    "localhost:8998"}})
+
+(def datomic-config
+  { :datomic-name "jobtech-taxonomy-development"  ;; "jobtech-taxonomy-production"
+   :datomic-cfg {
+                 :server-type :ion
+                 :region "eu-west-1" ;; e.g. us-east-1
+                 :system "prod-jobtech-taxonomy-db"
+                 ;;:creds-profile "<your_aws_profile_if_not_using_the_default>"
+                 :endpoint "http://entry.prod-jobtech-taxonomy-db.eu-west-1.datomic.net:8182/"
+                 :proxy-port 8182
+                 :timeout 6000000
+                 }
+   }
+  )
+
 
 (def ^:private legacydb-config
   {:subprotocol "mssql"
