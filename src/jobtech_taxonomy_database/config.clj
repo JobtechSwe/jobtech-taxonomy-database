129e71dff21c7843a7a8c57b3bf9a7f38e52add9
Henrik Suzuki <6067421+qeshi@users.noreply.github.com>
2019-04-30 17:43:29 +0200

    changing default database to henrik

--- src/jobtech_taxonomy_database/config.clj
+++ src/jobtech_taxonomy_database/config.clj
@@ -5,21 +5,26 @@
 ;;  ... (delay (load-file (.getFile (resource "config.clj"))))
 
 #_(def ^:private datomic-config
-    {:datomic-name "taxonomy_v13"
-     :datomic-cfg {:server-type :peer-server
-                   :access-key  "myaccesskey"
-                   :secret      "mysecret"
-                   :endpoint    "localhost:8998"}})
+  {:datomic-name "taxonomy_v13"
+   :datomic-cfg {:server-type :peer-server
+                 :access-key  "myaccesskey"
+                 :secret      "mysecret"
+                 :endpoint    "localhost:8998"}})
 
 (def datomic-config
-  {:datomic-name "jobtech-taxonomy-production"   ;;"jobtech-taxonomy-per-dev"  ;; "jobtech-taxonomy-development" "jobtech-taxonomy-production"
-   :datomic-cfg {:server-type :ion
+  { :datomic-name  "jobtech-taxonomy-henrik-dev" ;; "jobtech-taxonomy-developement"  ;; "jobtech-taxonomy-production"
+   :datomic-cfg {
+                 :server-type :ion
                  :region "eu-west-1" ;; e.g. us-east-1
                  :system "prod-jobtech-taxonomy-db"
                  ;;:creds-profile "<your_aws_profile_if_not_using_the_default>"
                  :endpoint "http://entry.prod-jobtech-taxonomy-db.eu-west-1.datomic.net:8182/"
                  :proxy-port 8182
-                 :timeout 6000000}})
+                 :timeout 6000000
+                 }
+   }
+  )
+
 
 (def ^:private legacydb-config
   {:subprotocol "mssql"
