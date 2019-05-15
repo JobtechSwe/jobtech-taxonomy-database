5f233be5423e1efc27827495793735d64f2eba1c
SickanEkman <sara.d.ekman@gmail.com>
2019-05-15 17:07:06 +0200

    Rafactor: use converter utils for converting concept.

--- src/jobtech_taxonomy_database/converters/driving_licence_converter.clj
+++ src/jobtech_taxonomy_database/converters/driving_licence_converter.clj
@@ -2,7 +2,8 @@
   (:gen-class)
   (:require [jobtech-taxonomy-database.legacy-migration :as legacy-migration]
             [jobtech-taxonomy-database.converters.nano-id-assigner :as nano]
-            [jobtech-taxonomy-database.converters.converter-util :as util]
+            [jobtech-taxonomy-database.converters.converter-util :as u]
+            [jobtech-taxonomy-database.types :as t]
             ))
 
 
@@ -11,7 +12,7 @@
         temp-id-1 (str "driving-licence-combination-" kombinationsid)
         temp-id-2 (str "driving-license-" körkortsid)
         ]
-    (util/create-relation temp-id-1 temp-id-2 :hyperonym)
+    (u/create-relation temp-id-1 temp-id-2 :hyperonym)
     )
   )
 
@@ -24,8 +25,8 @@
     (concat
      relations
      [
-      (util/create-concept nano-id temp-id temp-id temp-id :driving-licence-combination driving-licence-id)
-      (util/create-term nano-id temp-id)
+      (u/create-concept nano-id temp-id temp-id temp-id :driving-licence-combination driving-licence-id)
+      (u/create-term nano-id temp-id)
      ]
      )
     )
@@ -34,11 +35,20 @@
 
 (defn converter
   "Immutable driving licence converter."
-  [data]
-  (let [category-67 :driving-licence                ;json-nyckeln
-        id-67 (str (:drivinglicenceid data))        ;ska matcha legacyAmsTaxonomyId i json
-        description-67 (:term data)]                ;ska matcha preferredTerm i json
-    (let [nano-id (nano/get-nano category-67 (keyword id-67))]
+;  (let [type :driving-licence                ;json-nyckeln
+;        id-67 (str (:drivinglicenceid data))        ;ska matcha legacyAmsTaxonomyId i json
+;        description-67 (:term data)]                ;ska matcha preferredTerm i json
+    ;(let
+      ;[nano-id (nano/get-nano category-67 (keyword id-67))]
+
+      [{:keys [term description drivinglicenceid]}]
+      {:pre [term description drivinglicenceid]}
+      (let
+        [concept (u/create-concept t/occupation-field term description drivinglicenceid)]
+        [concept (u/create-term (:concept/id concept) term)]
+        ))
+
+#_
       [{:db/id                                          (str "driving-licence-" id-67)
         :concept/id                                     nano-id
         :concept/description                            (:description data)
@@ -49,7 +59,7 @@
         :concept.external-standard/driving-licence-code (:drivinglicencecode data)
         }
        {:db/id          nano-id
-        :term/base-form description-67}])))
+        :term/base-form description-67}];;)))
 
 
 
