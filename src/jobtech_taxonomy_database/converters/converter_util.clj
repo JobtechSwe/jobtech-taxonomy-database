09d2f706c2e6b6e16cd21edb6c2d1601e5e1abcb
SickanEkman <sara.d.ekman@gmail.com>
2019-05-15 17:05:55 +0200

    Small refactoring + add ns doc

--- src/jobtech_taxonomy_database/converters/converter_util.clj
+++ src/jobtech_taxonomy_database/converters/converter_util.clj
@@ -1,4 +1,6 @@
-(ns jobtech-taxonomy-database.converters.converter-util
+(ns ^{:author "Batfish"
+      :doc "utils for converting SQL data to Datomic"}
+  jobtech-taxonomy-database.converters.converter-util
   (:gen-class)
   (:require [datomic.client.api :as d]
             [jobtech-taxonomy-database.legacy-migration :as legacy-migration]
@@ -6,33 +8,36 @@
             [jobtech-taxonomy-database.datomic-connection :refer :all :as conn]
             [camel-snake-kebab.core :as csk]
             [clojure.spec.alpha :as s]
-            [jobtech-taxonomy-database.types :as t]
-            ))
+            [jobtech-taxonomy-database.types :as t]))
 
 
-(defn get-concept-id [instance-type legacy-id]
-  (nano/get-nano (csk/->kebab-case-string instance-type) (str legacy-id))
-  )
+(defn- get-concept-id [instance-type legacy-id]
+  "- Checks if the instance-type has a valid value
+  - Returns nano-id"
+  (nano/get-nano (csk/->kebab-case-string instance-type) (str legacy-id)))
+
 
 (defn get-temp-id [instance-type legacy-id]
-  (str (csk/->kebab-case-string instance-type)  "-" legacy-id)
-  )
+  (str (csk/->kebab-case-string instance-type)  "-" legacy-id))
 
-(defn get-concept-id-and-temp-id [instance-type legacy-id]
-  [ (get-concept-id instance-type legacy-id)
-    (get-temp-id instance-type legacy-id)
-   ]
-  )
+
+(defn- get-concept-id-and-temp-id [instance-type legacy-id]
+  [(get-concept-id instance-type legacy-id)
+   (get-temp-id instance-type legacy-id)])
+
 
 (defn create-concept
   ([instance-type label description legacy-id]
-   (let [[concept-id temp-id] (get-concept-id-and-temp-id instance-type legacy-id)
-         ]
-     (create-concept concept-id temp-id label description instance-type legacy-id)
-     )
-   )
-  ([concept-id temp-id label description instance-type legacy-ams-db-id]
-   {:pre [(s/valid? ::t/concept-types instance-type)]}
+   (let [[concept-id temp-id] (get-concept-id-and-temp-id instance-type legacy-id)]
+     (create-concept instance-type label description legacy-id concept-id temp-id)))
+  ([instance-type label description legacy-id concept-id temp-id]
+   {:pre [
+          (s/valid? ::t/concept-types instance-type)
+          (s/valid? string? concept-id)
+          (s/valid? string? temp-id)
+          (s/valid? string? label)
+          (s/valid? string? description)
+          (or (s/valid? string? legacy-id)(s/valid? int? legacy-id))]}
    {:db/id                                        temp-id
     :concept/id                                   concept-id
     :concept/description                          description
@@ -41,31 +46,32 @@
     :concept/alternative-terms                    #{concept-id}
     :concept/instance-type                         instance-type
     :concept/category                             (csk/->kebab-case-keyword instance-type) ; deprecated this one
-    :concept.external-database.ams-taxonomy-67/id (str legacy-ams-db-id);; TODO rename attribute
-    })
-  )
+    :concept.external-database.ams-taxonomy-67/id (str legacy-id);; TODO rename attribute
+    }))
+
 
 (defn create-term [nano-id term]
   {:db/id          nano-id
-   :term/base-form term}
-  )
+   :term/base-form term})
+
 
 (defn create-term-from-concept [ {:keys  [:concept/id :concept/preferred-label ]}]
+  "never mind this, it's not going to be used"
   {:db/id id
-   :term/base-form preferred-label}
-  )
+   :term/base-form preferred-label})
+
 
 (defn create-relation [concept1 concept2 type]
+  "concept1 should be an entity-id OR temp-id"
   {:pre [(s/valid? ::t/relation-types type)]}
   {:relation/concept-1 concept1
    :relation/concept-2 concept2
-   :relation/type type
-   }
-  )
+   :relation/type type})
+
 
 (defn create-broader-relation-to-concept [concept broader-temp-id]
-  (create-relation (:db/id concept) broader-temp-id t/broader )
-  )
+  "inputs should be entity-ids OR temp-ids"
+  (create-relation (:db/id concept) broader-temp-id t/broader ))
 
 
 (def get-concept-by-legacy-id-query '[:find ?s
@@ -75,21 +81,31 @@
                                       [?s :concept/category ?category]
                                       ])
 
+#_
+(def get-entity-id-query
+  "This function should replace the above function (get-concept-by-legacy-id-query) in the future.
+  Call function with legacy-id and type"
+    '[:find ?s
+    :in $ ?legacy-id ?type
+    :where
+    [?s :concept.external-database.ams-taxonomy-67/id ?legacy-id]
+    [?s :concept/type ?type]
+    ])
+
 (defn get-concept-by-legacy-id [legacy-id category]
-  (ffirst (d/q get-concept-by-legacy-id-query (conn/get-db) (str legacy-id) category))
-  )
+  (ffirst (d/q get-concept-by-legacy-id-query (conn/get-db) (str legacy-id) category)))
 
 
 (def get-concept-by-attribute-and-value-query '[:find ?s
                                       :in $ ?attribute ?value ?category
                                       :where
                                       [?s ?attribute ?value]
-                                      [?s :concept/category ?category]
-                                      ])
+                                      [?s :concept/category ?category]])
+
 
 (defn get-concept-by-attribute-and-value [attribute value category]
-  (ffirst (d/q get-concept-by-attribute-and-value-query (conn/get-db) attribute value category))
-  )
+  (ffirst (d/q get-concept-by-attribute-and-value-query (conn/get-db) attribute value category)))
+
 
 (def get-preferred-term-and-enity-id-by-legacy-id-query '[:find ?term ?s
                                       :in $ ?legacy-id ?category
@@ -97,13 +113,11 @@
                                       [?t :term/base-form ?term]
                                       [?s :concept/preferred-term ?t]
                                       [?s :concept.external-database.ams-taxonomy-67/id ?legacy-id]
-                                      [?s :concept/category ?category]
-                                      ])
+                                      [?s :concept/category ?category]])
+
 
 (defn get-preferred-term-by-legacy-id [legacy-id category]
-  (first (d/q get-preferred-term-and-enity-id-by-legacy-id-query (conn/get-db) legacy-id category))
-  )
-
+  (first (d/q get-preferred-term-and-enity-id-by-legacy-id-query (conn/get-db) legacy-id category)))
 
 
 (defn get-concept [entity-id]
@@ -112,24 +126,20 @@
                           {:concept/preferred-term [:db/id :term/base-form]}
                           :concept/category
                           :concept.external-database.ams-taxonomy-67/id]
-          entity-id)
-  )
+          entity-id))
 
 
 (defn get-entity-if-exists-or-temp-id [legacy-id category]
   (if-let [entity-id (get-concept-by-legacy-id legacy-id category)]
     entity-id
-    (str  (name category) "-" legacy-id)
-    )
-  )
+    (str  (name category) "-" legacy-id)))
 
 
 (defn deprecate-concept [category legacy-id]
   {:pre [legacy-id category]}
   (if-let [entity-id (get-concept-by-legacy-id (str legacy-id) category)]
-    {:db/id entity-id  :concept/deprecated true  }
-    )
-  )
+    {:db/id entity-id  :concept/deprecated true  }))
+
 
 (defn update-preferred-term [new-preferred-term old-preferred-term-id entity-id]
   "refactor this later when all concepts has alternative-terms"
@@ -139,7 +149,4 @@
      {:db/id entity-id
       :concept/description new-preferred-term
       :concept/preferred-term temp-id
-      :concept/alternative-terms [old-preferred-term-id]
-      }
-     ])
-  )
+      :concept/alternative-terms [old-preferred-term-id]}]))
