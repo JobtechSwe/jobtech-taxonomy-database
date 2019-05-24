3b962aae780de60b5e965c9cef575e759483cb62
SickanEkman <sara.d.ekman@gmail.com>
2019-05-24 10:17:04 +0200

    Refactor

--- src/jobtech_taxonomy_database/converters/skills_converter_new_changes.clj
+++ src/jobtech_taxonomy_database/converters/skills_converter_new_changes.clj
@@ -8,25 +8,21 @@
             [jobtech-taxonomy-database.datomic-connection :refer :all :as conn]
             [jobtech-taxonomy-database.converters.nano-id-assigner :refer :all]
             [jobtech-taxonomy-database.converters.converter-util :as u]
-            [jobtech-taxonomy-database.types :as t]
-            ))
-
-;; (fetch-data get-new-skill)
-
+            [jobtech-taxonomy-database.types :as t]))
 
 (defn convert-new-skill [{:keys [id-68 term-68 skill-headline]}]
   {:pre [id-68 term-68 skill-headline]}
-  (if (not (u/get-concept-by-legacy-id id-68 t/skill ))
-    (let [ concept (u/create-concept t/skill term-68 term-68 id-68)]
-      [
-       concept
-       (u/create-term-from-concept concept)
-       (u/create-broader-relation-to-concept concept (u/get-entity-if-exists-or-temp-id skill-headline :skill-headline))
-       ]
-      )
-    )
-  )
-
+  (if (not (u/get-entity-id-by-legacy-id id-68 t/skill))
+    (let [concept (u/create-concept
+                    t/skill
+                    term-68
+                    term-68
+                    id-68)
+          concept-term (u/create-term-from-concept concept)
+          concept-relation (u/create-broader-relation-to-concept
+                             concept (u/get-entity-if-exists-or-temp-id skill-headline t/skill-headline))]
+      [concept concept-term concept-relation
+       ])))
 
 (defn convert-deprecated-skill [{:keys [id-67]}]
   {:pre [id-67]}()
@@ -35,11 +31,8 @@
 
 
 ;; TODO utred alternative-terms, behöver man lägga till den gamla termen i alternative-terms, finns i datomic historien så det går att slå upp tidigare preferred-term
-
-
-
 (defn convert-updated-skill [{:keys [skillid term]}]
-  (let [entity-id (u/get-concept-by-legacy-id skillid t/skill) ;; borde heta get-entity-id-by-legacy-i
+  (let [entity-id (u/get-entity-id-by-legacy-id skillid t/skill) ;; borde heta get-entity-id-by-legacy-i
         concept (u/get-concept  entity-id)
         old-preferred-term-id (get-in concept [:concept/preferred-term :db/id])
         ]
@@ -49,26 +42,19 @@
 
 
 (defn convert-replaced-skill [{:keys [skillid term skillidref]}]
-  (let [entity-id-old-skill (u/get-concept-by-legacy-id skillid t/skill)
-        entity-id-new-skill (u/get-concept-by-legacy-id skillidref t/skill)
+  (let [entity-id-old-skill (u/get-entity-id-by-legacy-id skillid t/skill)
+        entity-id-new-skill (u/get-entity-id-by-legacy-id skillidref t/skill)
         ]
     (when (and entity-id-old-skill entity-id-new-skill)
       {:db/id entity-id-old-skill
-       :concept/replaced-by entity-id-new-skill
-       }
-      )
-    )
-  )
-
+       :concept/replaced-by entity-id-new-skill})))
 
 (defn convert []
   (remove nil? (concat
                 (mapcat convert-new-skill (fetch-data get-new-skill))
                 (map convert-deprecated-skill (fetch-data get-deprecated-skill))
                 (mapcat convert-updated-skill (fetch-data get-updated-skill))
-                (map convert-replaced-skill (fetch-data get-replaced-skill))
-                ))
-  )
+                (map convert-replaced-skill (fetch-data get-replaced-skill)))))
 
 (comment
   {:db/id 16110044370254510,
