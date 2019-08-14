8c417b0ab4cf686cb7731fd957594cbd9ff39e48
Henrik Suzuki <6067421+qeshi@users.noreply.github.com>
2019-08-14 10:59:09 +0200

    changed version from 67 to 1

---
+++ src/jobtech_taxonomy_database/converters/version_0.clj
@@ -0,0 +1,7 @@
+(ns jobtech-taxonomy-database.converters.version-0)
+
+
+
+(defn convert []
+  [{:taxonomy-version/id 0}]
+  )
---
+++ src/jobtech_taxonomy_database/converters/version_2.clj
@@ -0,0 +1,7 @@
+(ns jobtech-taxonomy-database.converters.version-2)
+
+
+
+(defn convert []
+  [{:taxonomy-version/id 2}]
+  )
