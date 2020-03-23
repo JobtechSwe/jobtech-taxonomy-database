2d8ab8355e1c8a9a99d35462f4d4477263503186
Vladislav Protsenko <vladislav.protsenko@arbetsformedlingen.se>
2020-03-23 13:25:38 +0100

    Clean up database dumping process
    Previous state of database dumping was confusing to me. I worry there might be
    a good motivation to keep old dumps around, although it seems that a lot of
    old dumps around are unused. I removed all the unused dumps, keeping only
    bi-directional json mapping from and to new ids.
    
    The old way of db dumping used taxonomy_to_concept_v67.json file as a source of
    truth for some ids that were dumped from datomic. I removed it, since data in
    the newest database to get dumps from (jobtech-taxonomy-development-2020-03-18-2)
    is matching the data in v67.
    
    Previous dumping process also renamed all types to kebab-case, which is
    effectively noop on current types, so I removed that as well: it seems to be a
    relic of some old schema.

--- src/jobtech_taxonomy_database/converters/nano_id_assigner.clj
+++ src/jobtech_taxonomy_database/converters/nano_id_assigner.clj
@@ -1,56 +1,11 @@
 (ns jobtech-taxonomy-database.converters.nano-id-assigner
   (:gen-class)
-  (:require [cheshire.core :refer :all]
-            [clojure.string :as str]
+  (:require [cheshire.core :as json]
             [jobtech-taxonomy-database.nano-id :as nano]))
 
-(defn open-json
-  "Open json, return map with json keyword formatted as clojure keywords."
-  []
-  (parse-string (slurp "resources/right-taxonomy-dump-2019-12-02.json") true))
-
-(def taxonomy-67 (open-json))
-
-(defn get-json-category
-  "Return all data in the specific category "
-  [category]
-  (get-in taxonomy-67 [category]))
-
-(defn find-id
-  "Open json, lookup NANO-ID in {<category>:{<id>:{'conceptID': NANO-ID}}}"
-  [category id]
-  (get-in (get-json-category category) [id :conceptId]))
-
-(defn find-description
-  "Open json, lookup DESCRIPTION in {<category>:{<id>:{'preferredTerm': DESCRIPTION}}}"
-  [category id]
-  (get-in (get-json-category category) [id :preferredTerm]))
-
-(defn compare-ids
-  "Compare taxonomy-ID from sql and taxonomy-ID from json"
-  [category-67 id-67]
-  (let [id (find-id category-67 id-67)]
-    (if id id "no nano-id")))
-
-(defn compare-descriptions
-  "Compare concept description from sql and concept description from json"
-  [category-67 id-67 description-67]
-  (let [description (find-description category-67 id-67)]
-    (if (= description-67 description) "same description" "different description")))
+(def taxonomy
+  (json/parse-string (slurp "resources/taxonomy-to-concept.json") true))
 
 (defn get-nano [category id]
   {:pre [category id]}
-  (get-in taxonomy-67 [(keyword category) (keyword id) :conceptId] (nano/generate-new-id-with-underscore)))
-
-(defn append-line "" [file line]
-  (with-open [w (clojure.java.io/writer file :append true)]
-    (.write w line)))
-
-(defn get-nano-log-updates "writes to hardcoded filename atm"
-  [category id term]
-  (let* [lookup-id (get-in taxonomy-67 [(keyword category) (keyword id) :conceptId])]
-        (if lookup-id
-          lookup-id
-          (let [new-id (nano/generate-new-id-with-underscore)]
-            (append-line "resources/new-ids.json" (format "{ %s { %s { :preferredTerm \"%s\" :conceptId \"%s\" :type \"%s\" } } }\n" category id term new-id (str/replace category #"^:(.*)$" "$1")))
-            new-id))))
+  (get-in taxonomy [(keyword category) (keyword id) :conceptId] (nano/generate-new-id-with-underscore)))
