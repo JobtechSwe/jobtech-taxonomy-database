17ad2a742d0cd85f9fe1f27d8ddd67439c37c3a7
Ubuntu <ubuntu@ip-172-31-34-126.eu-west-1.compute.internal>
2020-05-18 07:29:36 +0000

    adding sun codes 2000 and utility functions for creating databases

--- src/jobtech_taxonomy_database/converters/SUN_education_field_legacy_converter.clj
+++ src/jobtech_taxonomy_database/converters/SUN_education_field_legacy_converter.clj
@@ -60,7 +60,7 @@
   (concat
    (convert-eductaion (lm/fetch-data lm/get-sun-field-1) t/sun-education-field-1 nil sun-legacy-key-prefix sun-field-key-2000)
    (convert-eductaion (lm/fetch-data lm/get-sun-field-2) t/sun-education-field-2 t/sun-education-field-1 sun-legacy-key-prefix sun-field-key-2000)
-   (convert-eductaion (lm/fetch-data lm/get-sun-field-3) t/sun-education-field-3 t/sun-education-field-2 sun-legacy-key-prefix  sun-field-key-2000)
+  (convert-eductaion (lm/fetch-data lm/get-sun-field-3) t/sun-education-field-3 t/sun-education-field-2 sun-legacy-key-prefix  sun-field-key-2000)
 
    (convert-eductaion (lm/fetch-data lm/get-sun-level-1) t/sun-education-level-1 nil sun-legacy-key-prefix sun-field-key-2000)
 
@@ -68,6 +68,6 @@
 
    (convert-eductaion (lm/fetch-data lm/get-sun-level-3) t/sun-education-level-3 t/sun-education-level-2 sun-legacy-key-prefix sun-field-key-2000)
 
-   (map convert-guide-tree (lm/fetch-data lm/get-sun-2000-guide))
+ (map convert-guide-tree (lm/fetch-data lm/get-sun-2000-guide))
 
-   ))
+  ))
--- src/jobtech_taxonomy_database/create_database.clj
+++ src/jobtech_taxonomy_database/create_database.clj
@@ -9,12 +9,30 @@
 
 
 
-(defn create-database-with-name [name]
-  (d/create-database (dc/get-client) {:db-name name})
+(defn get-timestamp []
+  (.format (java.text.SimpleDateFormat. "yyyy-MM-dd-mm-ss") (new java.util.Date))
   )
 
-;; (println "Usage: lein run -m jobtech-taxonomy-database.create-database 'my-database-name'")
+(defn create-database-name [type]
+  (str "jobtech-taxonomy-" type  "-" (get-timestamp))
+  )
+
+(defn create-database-with-name [type]
+  (let [name (create-database-name type)]
+    (do
+      (println "Creating Database")
+      (println name)
+      (d/create-database (dc/get-client) {:db-name name })))
+  )
+
 
 (defn -main [& args]
- (first *command-line-args*)
+  (let [name (first *command-line-args*)]
+    (when-not name
+      (binding [*out* *err*]
+        (println "Usage: lein run -m jobtech-taxonomy-database.create-database 'dev|frontend|prod'")
+        )
+      (System/exit 1))
+    (create-database-with-name name)
+    )
   )
